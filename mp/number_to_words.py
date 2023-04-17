083eaac278e3f4f7b38d5d327d46ef2a80feb858
Олег Грабчак ТР-14 <127214573+OlegHrab@users.noreply.github.com>
2023-04-17 22:44:44 +0300

    Update number_to_words.py

--- mp/number_to_words.py
+++ mp/number_to_words.py
@@ -1,5 +1,5 @@
 #Олег Грабчак ТР-14 2023
-from num2words import num2words
+from num2words import num2words#звертаємося до встановленого пакету
 
-def convert(number: int) -> str:
-    return num2words(number, lang='en')
+def convert(number: int):
+    return num2words(number, lang='uk')
