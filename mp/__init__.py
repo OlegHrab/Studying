149dd5b75f37ad929a9ef035adea18a3263c6c4f
Олег Грабчак ТР-14 <127214573+OlegHrab@users.noreply.github.com>
2023-06-08 20:37:06 +0300

    Delete mp directory

--- mp/__init__.py
+++
@@ -1,7 +0,0 @@
-from .number_to_words import convert
-
-def main():
-
-    number = int(input("Введіть число: "))
-    words = number_to_words.convert(number)
-    print(f"Ви ввели: {words}")
--- mp/number_to_words.py
+++
@@ -1,5 +0,0 @@
-#Олег Грабчак ТР-14 2023
-from num2words import num2words#звертаємося до встановленого пакету
-
-def convert(number: int):
-    return num2words(number, lang='uk')
